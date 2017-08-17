f91392e0edc035f1b78116e2803df8388b4ad0f6
Kostya Musienko <kmusienko97@gmail.com>
2017-08-18 01:57:40 +0300

    Removed unnecessary code.

--- src/main/java/com/ardas/task1/application/Runner.java
+++ src/main/java/com/ardas/task1/application/Runner.java
@@ -2,12 +2,11 @@ package com.ardas.task1.application;
 
 import com.ardas.task1.service.GreetingService;
 import com.ardas.task1.service.impl.GreetingServiceImpl;
-import java.io.IOException;
 import java.time.LocalTime;
 
 public class Runner {
 
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) {
     GreetingService greetingService = new GreetingServiceImpl();
     String greeting = greetingService.getGreetingByTime(LocalTime.now());
     System.out.println(greeting);
